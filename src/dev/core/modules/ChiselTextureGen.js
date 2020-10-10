93531b9638a596953e17ca380c83a3ffc6a27f49
ToxesFoxes <toxes_foxes@outlook.com>
2020-10-10 21:03:11 +0600

    Pre test code

--- src/dev/core/modules/ChiselTextureGen.js
+++ src/dev/core/modules/ChiselTextureGen.js
@@ -2,64 +2,63 @@ var Chisel = {
     assets: __dir__ + 'src/assets/mod_assets/',
     data: null,
     mods: {},
-    generators: {
-        default(block, variation) {
-
-        },
-        bookshelfGen(block, variation) {
-
-        }
-    },
+    generators: {},
 }
+
 Chisel.data = ResourceManager.ReadJSON(ResourceManager.Select(Chisel.assets, 'gen_blocks.json'))
 Chisel.gen_assets = Chisel.assets + 'gen_assets/'
 Chisel.terrain_atlas = Chisel.assets + 'terrain_atlas/'
 
-Chisel.genCtmTexture = function (srcPath, destPath) {
-    // TODO: CTM Bitmap manager
-}
-Chisel.genTexture = function (srcPath, destPath) {
-    // TODO: Bitmap manager
-}
-Chisel.readBlocks = function () {
-    if (Chisel.data)
-        for (let blockname in Chisel.data.blocks) {
-            let src = Chisel.gen_assets + blockname
-            let path = ResourceManager.getFilesList(src)
-            for (let key in path) { //TODO: rework this loop, and replace this to blockworker
-                let value = path[key]
-                switch (key) {
-                    case "variation":
-                        switch (value) {
-                            case "stone":
-                                let stone = Chisel.data.block_variations.stone
-                                for (let variation in stone) {
-                                    // if (stone[variation].search(/ctm[A-z]|ctm/)) // TODO: add ctm check
-                                    // TODO: Gen ctm textures
-                                    // TODO: Rewrite current ctm lib for chisel
-                                    ResourceManager.CopyToDest(src + '/' + stone[variation] + '.png', Chisel.terrain_atlas + blockname + variation + '.png')
-                                }
-                                break
-                        }
+Chisel.readBlocks = function (data) {
+    if (data) //reading object
+        for (let blockname in data.blocks) { // reading blocks from data
+            let block = data.blocks[blockname]
+            BlockWorker.genBlock(block, blockname) // generating block textures
+            // let path = ResourceManager.getFilesList(src)
+            // for (let key in path) { //TODO: rework this loop, and replace this to blockworker
+            //     let value = path[key]
 
-                        break
-                }
-            }
+            // }
         }
 }
-function BitmapWorker() { }
-function BlockWorker() { }
-function CTMWorker() { }//TODO: rework ctmlib for chisel
 
-BlockWorker.prototype.genBlock = function (block, name) {
+Chisel.addGenerator = function (generator, name) {
+    if (typeof generator !== 'function')
+        throw 'Cannot add generator function, generator is ' + typeof generator
+    if (typeof name !== 'string')
+        throw 'Cannot add generator with name type ' + typeof name
+    if (name in Chisel.generators)
+        throw 'Cannot add generator with name ' + name + ', generator is exists'
+
+    Chisel.generators[name] = generator
+}
+
+Chisel.addGenerator(
+    function (block, name) {
+        let src = Chisel.gen_assets + name
+        let path = ResourceManager.getFilesList(src)
+        for (let filename in path) {
+            alert(filename)
+        }
+    }, "default"
+)
+
+let BitmapWorker = {}
+let BlockWorker = {}
+let CTMWorker = {}//TODO: rework ctmlib for chisel
+
+BlockWorker.genBlock = function (block, name) {
     if (!block)
-        throw 'Cannot create undefined block with ' + (name ? name : undefined) + name
+        throw 'Cannot create undefined block with ' + (name ? name : undefined)
 
-    if (typeof block.subdirs === 'object' && !Array.isArray(block.subdirs)) {
-        for (let key in block)
-            Chisel.blockWorker(key[block], block)
-        return
+    if (Object.keys(block).length) {
+        block.register = false
+        throw 'Cannot create block ' + name + ', no properties found'
     }
+
+    if (typeof block.subdirs === 'object' && !Array.isArray(block.subdirs))
+        for (let key in block.subdirs)
+            Chisel.blockWorker(block.subdirs[key], key)
 
     if (typeof block.generator !== 'string' && block.generator)
         throw 'Cannot get custom generator for block ' + name + ', generator has invalid type=' + block.generator
@@ -69,7 +68,7 @@ BlockWorker.prototype.genBlock = function (block, name) {
 
     if (typeof block.require_mod === 'string')
         if (!Chisel.mods[block.require_mod])
-            return
+            throw 'Cannot add block with name ' + name + ', mod ' + block.require_mod + ' is not found'
 
     if (typeof block.register === 'undefined')
         block.register = true
@@ -84,12 +83,18 @@ BlockWorker.prototype.genBlock = function (block, name) {
 
     if (block.variation === 'custom' && !Array.isArray(block.variations))
         throw 'Custom variation is set, but variations format is not object'
-    //TODO: check all conditions and generate texture
+
+    if (!Chisel.generators[block.generator])
+        throw 'Cannot generate block with ' + block.generator + ', generator not registered'
+    else if (typeof Chisel.generators[block.generator] === 'function')
+        Chisel.generators[block.generator](block, name)
+    else throw 'Cannot execute generator ' + block.generator + ' for block ' + name + ', generator is ' + typeof Chisel.generators[block.generator]
 }
-BitmapWorker.prototype.genTexture = function (param) {
+BitmapWorker.genTexture = function (filePath, filename, destpath) {
     //TODO: create generating texture
+
 }
-BitmapWorker.prototype.genCTMTexture = function (param) {
+BitmapWorker.genCTMTexture = function (param) {
     //TODO: create generating texture
 }
 
--- src/dev/logic/register.js
+++
@@ -1,7 +0,0 @@
-ScriptingManager.readAllResources()
-
-BlockManager.registerList([
-    "test_block"
-])
-
-Game.dialogMessage(JSON.stringify(ScriptingManager.namespaces), "ScriptingManager.readAllResources")
