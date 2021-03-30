a45053a80ebfc891c9084ba87c95319673fc401d
ToxesFoxes <toxes_foxes@outlook.com>
2021-03-31 03:08:29 +0600

    Moved resources.ts

--- src/build/dev/register/resources.ts
+++ src/build/dev/register/resources.ts
@@ -1,22 +1,26 @@
-RM.mod = 'chisel'
-RM.resources = {
+
+const ModData = {
+	name: {
+		small: 'chisel',
+		normal: 'Chisel'
+	},
 	dirs: {
 		/** @returns mod assets path, ends with / */
-		assets: "resources/ic_mod/",
+		assets: 'resources/ic_mod/',
 		/** @returns java chisel resources path, ends with / */
-		chisel: "resources/pc_mod/chisel/",
+		chisel: 'resources/pc_mod/chisel/',
 		/** @returns java chisel guide path, ends with / */
-		chisel_guide: "resources/pc_mod/chisel_guide/",
+		chisel_guide: 'resources/pc_mod/chisel_guide/',
 		/** @returns item textures path, ends with / */
-		items_opaque: "resources/ic_mod/items-opaque/",
+		items_opaque: 'resources/ic_mod/items-opaque/',
 		/** @returns block textures path, ends with / */
-		terrain_atlas: "resources/ic_mod/terrain-atlas/",
+		terrain_atlas: 'resources/ic_mod/terrain-atlas/',
 		/** @returns sounds path, ends with / */
-		sound: "resources/ic_mod/sounds/",
+		sound: 'resources/ic_mod/sounds/',
 		/** @returns ui textures path, ends with / */
-		ui: "gui/ui/",
+		ui: 'gui/ui/',
 		/** @returns ui textures path, ends with / */
-		json: "json/"
+		json: 'json/'
 	},
 	files: {
 		test: 'json/test.json',
@@ -24,8 +28,4 @@ RM.resources = {
 		ctmtypes: 'json/ctmtypes.json',
 		blocks: 'json/blocks.json',
 	}
-}
-function OpenTile(tile_name: string): any {
-	let file = RM.Select(`${__dir__}${RM.resources.dirs.json}/tiles/${tile_name}.json`)
-	return RM.ReadJSON(file)
 }
