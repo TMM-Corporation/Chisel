2b2dd8764da8f8df3a6f221a5e239df105352a32
ToxesFoxes <toxes_foxes@outlook.com>
2021-04-07 03:06:41 +0600

    Some ItemName update

--- src/build/dev/lib/ItemName.ts
+++ src/build/dev/lib/ItemName.ts
@@ -1,5 +1,6 @@
 
 namespace ItemName {
+	var Data: { [id: number]: NameData }
 	export interface NameData {
 		inGui: string
 		inWorld: string
@@ -7,31 +8,28 @@ namespace ItemName {
 		normal: string
 		mod: string
 	}
-	export enum State {
-		None,
-		inGui,
-		inWorld
-	}
-	export var CurrentState = State.None
-	export class Manager {
+	export class Bind {
 		data: NameData
-		constructor(data: NameData, itemData: ItemInstance) {
+		itemData: ItemInstance
+		nameOverrideFunction: Callback.ItemNameOverrideFunction
+		constructor(data: NameData, itemData: ItemInstance, nameOverrideFunction: Callback.ItemNameOverrideFunction) {
 			this.data = data
-			Callback.addCallback("ItemNameOverride", (item: ItemInstance, translation: string, name: string) => {
-				if (itemData.id == item.id) {
-					switch (CurrentState) {
-						case State.inGui:
-							return this.data.normal + this.data.inGui + this.data.mod
-							break
-						case State.inWorld:
-							return this.data.normal + this.data.inWorld
-							break
-						default:
-							return this.data.normal
-							break
-					}
+			this.itemData = itemData
+			this.nameOverrideFunction = nameOverrideFunction
+		}
+		setCustomNameOverrideFunction(item: ItemInstance, translation: string, name: string) {
+			// Item.registerNameOverrideFunction(itemData.id, (itemC: ItemInstance, translationC: string, nameC: string) => {
+				switch (MCGUI.Screen.Latest.enumType) {
+					case MCGUI.Screen.Types.ENTITY:
+					case MCGUI.Screen.Types.NATIVE_TILE:
+					case MCGUI.Screen.Types.INVENTORY:
+						return this.data.normal + this.data.inGui + this.data.mod
+					case MCGUI.Screen.Types.GAMEPLAY:
+						return this.data.normal + this.data.inWorld
+					default:
+						return this.data.normal
 				}
-			})
+			// })
 		}
 		setBaseName(value: string) {
 			this.data.normal = value
@@ -40,7 +38,7 @@ namespace ItemName {
 			this.data.toolTip = value
 		}
 		setModName(value: string) {
-			this.data.toolTip = `${Native.Color.DARK_PURPLE}${value}`
+			this.data.mod = `${Native.Color.DARK_PURPLE}${value}`
 		}
 		setInGuiName(value: string) {
 			this.data.inGui = value
@@ -48,19 +46,9 @@ namespace ItemName {
 		setInWorldName(value: string) {
 			this.data.inWorld = value
 		}
+		get default() {
+			return this.data.normal
+		}
 	}
-	var latestName = ""
-	Callback.addCallback("NativeGuiChanged", (name, lastName, isPushEvent) => {
-		if (name == "in_game_play_screen")
-			CurrentState = State.inWorld
-		else if (lastName == "in_game_play_screen" && isPushEvent)
-			CurrentState = State.inGui
 
-		if (lastName != "")
-			latestName = lastName
-		else
-			lastName = latestName
-
-		console.info(`${isPushEvent ? lastName + " --> " + name : name + " <--" + lastName} (${isPushEvent ? "pushed" : "popped"}) (${CurrentState == State.inGui ? "inGui" : CurrentState == State.inWorld ? "inWorld" : "None"})`, `[ItemName.ts] ItemName Callback.NativeGuiChanged`)
-	})
 }
