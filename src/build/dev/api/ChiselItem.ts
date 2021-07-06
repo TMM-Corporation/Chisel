28d40420b86d295da5b96fd3ca7a0cf6dea1fbe4
ToxesFoxes <toxes_foxes@outlook.com>
2021-07-07 01:58:56 +0600

    Removed using modes from ChiselItem

--- src/build/dev/api/ChiselItem.ts
+++ src/build/dev/api/ChiselItem.ts
@@ -14,21 +14,6 @@ namespace ChiselItem {
 		Carving,
 		OpenGui
 	}
-	export enum UseMode {
-		/* Chisel a 3x1 column of blocks */
-		Column,
-		/* Chisel an area of alike blocks, extending 10 blocks in any direction */
-		Contiguous,
-		/* Chisel an area of alike blocks, extending 10 blocks along the plane of the current side */
-		Contiguous_2D,
-		/* Chisel a 3x3 square of blocks */
-		Panel,
-		/* Chisel a 1x3 row of blocks */
-		Row,
-		/* Chisel a single block */
-		Single,
-	}
-
 	export interface ItemData {
 		id?: number
 		durability: number
