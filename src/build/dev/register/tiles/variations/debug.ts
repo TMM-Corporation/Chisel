40389b20fa4b06745250af05c9788b8b988e2443
ToxesFoxes <toxes_foxes@outlook.com>
2021-07-12 04:48:06 +0600

    Splitting groups by files

--- src/build/dev/register/tiles/variations/debug.ts
+++ src/build/dev/register/tiles/variations/debug.ts
@@ -31,34 +31,3 @@ debugGroup.addFromDescription({
 		}
 	]
 })
-let stoneGroup = new Carvable.Group("stone_group", "Stone Group")
-stoneGroup.addFromDescription({
-	name: "stone",
-	variations: [
-		{
-			block: {
-				id: 1, data: 0
-			},
-		},
-		{
-			block: {
-				id: 98, data: 0
-			}
-		},
-		{
-			block: {
-				id: 98, data: 1
-			}
-		},
-		{
-			block: {
-				id: 98, data: 2
-			}
-		},
-		{
-			block: {
-				id: 98, data: 3
-			}
-		}
-	]
-})
