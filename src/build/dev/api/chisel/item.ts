2cdd31fc8424f39b19fa27f2d06ac4478fd416b7
ToxesFoxes <toxes_foxes@outlook.com>
2021-07-12 22:49:54 +0600

    Added some comments and some refactoring of ChiselItem

--- src/build/dev/api/chisel/item.ts
+++ src/build/dev/api/chisel/item.ts
@@ -10,18 +10,58 @@ namespace ChiselItem {
 	export namespace Data {
 		interface Structure { [key: string]: IChiselItem }
 		export const chiselItems: Structure = {}
-		export function registerItem(namedID: string, description: IChiselItem) {
-			chiselItems[namedID] = description
+		
+		export namespace ItemRegistry {
+			/**
+			 * Adds to registered items chisel item with description
+			 * @param namedID item string id
+			 * @param description chisel description
+			 * @returns boolean if item successfully registered
+			 */
+			export function register(namedID: string, description: IChiselItem): boolean {
+				const registered = isRegistered(namedID)
+				if (!registered) {
+					chiselItems[namedID] = description
+					console.info(`Successfully registered chisel item '${namedID}'`)
+				} else console.warn(`Cannot register chisel item '${namedID}', because it is already registered`)
+
+				return !registered
+			}
+			/**
+			 * Removes from registered items chisel item
+			 * @param namedID item string id
+			 * @returns boolean if item successfully unregistered
+			 */
+			export function unregister(namedID: string): boolean {
+				const registered = isRegistered(namedID)
+				if (registered) {
+					delete chiselItems[namedID]
+					console.info(`Successfully unregistered chisel item '${namedID}'`)
+				} else console.warn(`Cannot unregister chisel item '${namedID}', because it is not registered`)
+
+				return registered
+			}
+			/**
+			 * Determines whether registered is item as chisel by string id
+			 * @param namedID item string id
+			 * @returns item is registered as chisel
+			 */
+			export function isRegistered(namedID: string): boolean {
+				return namedID in chiselItems
+			}
+			/**
+			 * Gets description by name for chisel item
+			 * @param namedID item string id
+			 * @returns chisel description
+			 */
+			export function getDescriptionByName(namedID: string): IChiselItem {
+				return chiselItems[namedID]
+			}
 		}
-		export function unregisterItem(namedID: string) {
-			delete chiselItems[namedID]
-		}
-		export function isRegistered(namedID: string) {
-			return namedID in chiselItems
-		}
-		export function getDescriptionByName(namedID: string) {
-			return chiselItems[namedID]
-		}
+		/**
+		 * Prevents destroy block when player hand chisel item
+		 * @param playerUID client uid
+		 */
 		function preventDestroy(playerUID: number) {
 			for (const name in chiselItems) {
 				if (Object.prototype.hasOwnProperty.call(chiselItems, name)) {
@@ -31,13 +71,33 @@ namespace ChiselItem {
 				}
 			}
 		}
+		/**
+		 * Initialization of all callbacks
+		 */
 		export function init() {
 			Callback.addCallback("DestroyBlock", (c, tile, playerUID) => preventDestroy(playerUID))
 			Callback.addCallback("DestroyBlockStart", (c, tile, playerUID) => preventDestroy(playerUID))
 		}
 	}
-
 	export namespace Controller {
+		/**
+		 * Object representing Item damage
+		 */
+		interface ItemDamage {
+			appliedDamage: number
+			breaked: boolean
+			used: boolean
+			playerGM: number
+		}
+		/**
+		 * Object representing Carving results
+		 */
+		export interface ICarveResults {
+			id: number
+			data: number
+			sound: string
+			change: boolean
+		}
 		export function createChiselItem(item: IChiselItem, extra?: ItemExtraData): { nativeItem: Item.NativeItem, id: number } {
 			let id = IDRegistry.genItemID(item.namedID)
 			let nativeItem = Item.createItem(item.namedID, item.name, item.texture, { stack: 1, isTech: extra ? true : false })
@@ -47,17 +107,29 @@ namespace ChiselItem {
 			if (item.durability)
 				Item.setMaxDamage(id, item.durability)
 
+			Data.ItemRegistry.register(item.namedID, item)
 			return { nativeItem, id }
 		}
-		export function isHandleChisel(item: ItemInstance, chisel: IChiselItem) {
-			return item.id == chisel.numericID
+		/**
+		 * Determines whether the player is holding a chisel
+		 * @param item ItemInsance of current item
+		 * @param chiselItem registered chisel description
+		 * @returns  
+		 */
+		export function isHandleChisel(item: ItemInstance, chiselItem: IChiselItem) {
+			return item.id == chiselItem.numericID
 		}
-		export interface ICarveResults { id: number, data: number, sound: string, change: boolean }
-		export function getCarvingBlock(player: number, tile: Tile): ICarveResults {
+		/**
+		 * Gets carving block from current tile and chisel extra data
+		 * @param playerUID client uid
+		 * @param tile current touched tile [id:data]
+		 * @returns carving block results
+		 */
+		export function getCarvingBlock(playerUID: number, tile: Tile): ICarveResults {
 			let defaultResult = { id: -1, data: -1, sound: "chisel.fallback", change: false }
 			let finalResult = { id: -1, data: -1, sound: "chisel.fallback", change: false }
 			let searchDecoded, sneaking, extra = { id: -1, data: -1 }
-			let item = Entity.getCarriedItem(player)
+			let item = Entity.getCarriedItem(playerUID)
 			if (item.extra)
 				extra = { id: item.extra.getInt("variationId"), data: item.extra.getInt("variationData") }
 
@@ -74,7 +146,7 @@ namespace ChiselItem {
 			if (extra.id == -1 && tileSearch) {
 				let tileSearchResult = tileSearch.result
 				searchDecoded = Carvable.Groups.idDataFromSearch(tileSearchResult)
-				sneaking = Entity.getSneaking(player)
+				sneaking = Entity.getSneaking(playerUID)
 				let { prev, next } = { prev: tileSearchResult.prev, next: tileSearchResult.next }
 				let result = sneaking &&
 					prev ? searchDecoded.prev :
@@ -127,29 +199,48 @@ namespace ChiselItem {
 			console.warn(`${JSON.stringify(finalResult)}, some shit happend, [${JSON.stringify(groups[0])}, ${JSON.stringify(groups[1])}] [${extra.id}:${extra.data} | ${tile.id}:${tile.data}]`)
 			return finalResult
 		}
-		export function carveBlock(coords: Callback.ItemUseCoordinates, item: ItemInstance, tile: Tile, player: number, chiselItem: IChiselItem): boolean {
+		/**
+		 * Carves current block in specified coords with current item
+		 * @param coords coords where item used
+		 * @param item ItemInstance of current item
+		 * @param tile current touched tile [id:data]
+		 * @param playerUID client uid 
+		 * @param chiselItem registered chisel description
+		 * @returns boolean if carving block successfull or not 
+		 */
+		export function carveBlock(coords: Callback.ItemUseCoordinates, item: ItemInstance, tile: Tile, playerUID: number, chiselItem: IChiselItem): boolean {
 			if (!isHandleChisel(item, chiselItem))
 				return false
-			return ChiselModes.carveBlocks(coords, item, tile, player)
-			return false
+			return ChiselModes.carveBlocks(coords, item, tile, playerUID)
 		}
-		export function breakItem(player: number) {
-			Entity.setCarriedItem(player, 0, 0, 0)
-			SoundManager.playSoundAtEntity(player, "item_break", 1, getRandomArbitrary(0.85, 1))
-			return true
+		/**
+		 * Breaks carried item and playing "item breaked" sound
+		 * @param playerUID client uid
+		 */
+		export function breakItem(playerUID: number): void {
+			Entity.setCarriedItem(playerUID, 0, 0, 0)
+			SoundManager.playSoundAtEntity(playerUID, "item_break", 1, getRandomArbitrary(0.85, 1))
+
 		}
-		export function onUse(player: number, item: ItemInstance, damage: number = 1): { appliedDamage: number, breaked: boolean, used: boolean, playerGM: number } {
+		/**
+		 * Processes item damage
+		 * @param playerUID client uid
+		 * @param item ItemInstance of handle item
+		 * @param [damage] the amount of damage that should be applied
+		 * @returns ItemDamage data
+		 */
+		export function onUse(playerUID: number, item: ItemInstance, damage: number = 1): ItemDamage {
 			let maxDamage = Item.getMaxDamage(item.id)
-			let playerGM = new PlayerActor(player).getGameMode()
+			let playerGM = new PlayerActor(playerUID).getGameMode()
 			let breaked = false
 			var available = maxDamage - item.data
 			var appliedDamage = damage <= available ? damage : available
 
 			if (playerGM == 0) item.data += appliedDamage
 			if (item.data >= maxDamage)
-				breaked = this.breakItem(player)
+				breaked = this.breakItem(playerUID)
 			else
-				Entity.setCarriedItem(player, item.id, 1, item.data, item.extra)
+				Entity.setCarriedItem(playerUID, item.id, 1, item.data, item.extra)
 			console.info(`Data: [${item.data}/${maxDamage}], GameMode: ${playerGM}, damage/applied: [${damage}/${appliedDamage}]`, `[ChiselItem.ts] ChiselItem.Custom.onUse`)
 			return {
 				appliedDamage: (playerGM == 0 ? appliedDamage : 0),
@@ -171,6 +262,10 @@ namespace ChiselItem {
 				Controller.carveBlock(c, item, tile, player, description)
 			})
 		}
+		/**
+		 * Builds default extra for creative item
+		 * @returns default extra of chisel item
+		 */
 		buildDefaultExtra(): ItemExtraData {
 			var extra = new ItemExtraData()
 			extra.putString("containerUID", 'null')
@@ -180,13 +275,19 @@ namespace ChiselItem {
 			extra.putInt("carveMode", 0)
 			return extra
 		}
+		/**
+		 * Opens gui for client when not sneaking
+		 * @param item ItemInstance of current item to setup container and uid
+		 * @param playerUID client uid 
+		 * @returns true if gui is registered for chisel item
+		 */
 		openGUI(item: ItemInstance, playerUID: number): boolean {
 			const client = Network.getClientForPlayer(playerUID)
 			if (!client)
 				return false
 
-			var container: ItemContainer = this.setupContainer(item, playerUID)
-			var desc = this.description
+			const container: ItemContainer = this.setupContainer(item, playerUID)
+			const desc = this.description
 			// if (Controller.isHandleChisel(item, desc))
 			if (!Entity.getSneaking(playerUID))
 				if (desc.gui) {
@@ -195,6 +296,12 @@ namespace ChiselItem {
 				}
 			return false
 		}
+		/**
+		 * Setups server container and server side window container
+		 * @param item ItemInstance of current item to setup container and uid
+		 * @param playerUID client uid
+		 * @returns server side ItemContainer 
+		 */
 		setupContainer(item: ItemInstance, playerUID: number) {
 			const cUID: string = this.getChiselExtraData(playerUID, item)
 			const container: ItemContainer = ChiselGUI.Data.getContainerByUID(cUID).container
@@ -204,6 +311,14 @@ namespace ChiselItem {
 
 			return container
 		}
+
+		/**
+		 * Creating item extra data for container.
+		 * BUG: catch extra data when new item is created, found why new item has old cUID
+		 * @param player - player entity id
+		 * @param item - item
+		 * @returns container id for this item
+		 */
 		getChiselExtraData(player: number, item: ItemInstance): string {
 			if (!item.extra)
 				item.extra = new ItemExtraData()
