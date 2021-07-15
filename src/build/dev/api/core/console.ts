f731e590d1c20e48d0e49978e6cbf32c3c0ba38d
ToxesFoxes <toxes_foxes@outlook.com>
2021-07-15 22:33:50 +0600

    Prefix and Message builders for console.log

--- src/build/dev/api/core/console.ts
+++ src/build/dev/api/core/console.ts
@@ -1,26 +1,32 @@
 namespace console {
+	function buildPrefix(prefix?: string) {
+		return prefix ? "[" + prefix + "] " : ""
+	}
+	function buildMessage(message: any, prefix?: string) {
+		return buildPrefix(prefix) + message
+	}
 	export function genTSDocFor(data: any, name: string) {
 		Logger.Log(`\n${Dumper.toDTS(data, name)}`, 'DOCUMENTATION')
 	}
 	export function debug(message: any, prefix?: string) {
-		Logger.Log(`${prefix ? "[" + prefix + "] " : ""}${message}`, 'DEBUG')
+		Logger.Log(`${message}`, 'DEBUG')
 	}
 	export function json(message: any, prefix?: string) {
-		Logger.Log(`${prefix ? "[" + prefix + "] " : ""}${JSON.stringify(message)}`, 'JSON' + (prefix ? "-" + prefix : ""))
+		Logger.Log(buildMessage(JSON.stringify(message), prefix), 'JSON' + (prefix ? "-" + prefix : ""))
 	}
 	export function error(message: string | number, prefix?: string) {
-		Logger.Log(`${prefix ? "[" + prefix + "] " : ""}${message}`, 'ERROR')
+		Logger.Log(buildMessage(message, prefix), 'ERROR')
 	}
 	export function exception(exception: java.lang.Throwable) {
 		Logger.LogError(exception)
 	}
 	export function info(message: string | number, prefix?: string) {
-		Logger.Log(`${prefix ? "[" + prefix + "] " : ""}${message}`, 'INFO')
+		Logger.Log(buildMessage(message, prefix), 'INFO')
 	}
 	export function log(message: string | number, prefix?: string) {
-		Logger.Log(`${prefix ? "[" + prefix + "] " : ""}${message}`, 'LOG')
+		Logger.Log(buildMessage(message, prefix), 'LOG')
 	}
 	export function warn(message: string | number, prefix?: string) {
-		Logger.Log(`${prefix ? "[" + prefix + "] " : ""}${message}`, 'WARN')
+		Logger.Log(buildMessage(message, prefix), 'WARN')
 	}
 }
